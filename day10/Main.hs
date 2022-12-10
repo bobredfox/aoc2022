08ed9cdca9f2aaa7bf186c38a86ddcb9a4bb1b3a
bobredfox <gelberwolf@posteo.de>
2022-12-10 21:12:18 +0100

    Completed part 2

--- day10/Main.hs
+++ day10/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import Control.Monad.Writer
 
 data Command = AddX Int | Noop  deriving (Show)
 
@@ -27,13 +26,32 @@ modifyCommands (x:xs) = [x] <> modifyCommands xs
 buildSum :: [(Int,Int)] -> Int
 buildSum x = sum $ map (\y -> fst y * snd y) $ filter (\y -> elem (snd y) [20,60,100,140,180,220]) x
 
+getRows :: [(Int,Int)] -> [[(Int,Int)]]
+getRows [] = []
+getRows xs = [take 40 xs] <> getRows (drop 40 xs) 
+
+
+buildPixel :: Int -> Int -> Bool
+buildPixel pixel x = min <= pixel && max >= pixel 
+    where
+        min = x - 1
+        max = x + 1
+
+buildRow :: [(Int,Int)] -> String
+buildRow xs = map (\x -> case buildPixel (fst x) (snd x) of
+                           True -> '#'
+                           False -> '.') remap 
+        where
+            remap = zip [0..39] (map (fst) xs)
 
 main :: IO()
 main = do
     input <- readFile "input.txt"
     let commands = modifyCommands $ map (parseInput) $ lines input -- [Commands]
     let part1 = scanl (\acc x -> processCommands acc x) (1,1) commands
+    let part2 = getRows part1
     print $ buildSum part1
+    mapM_ putStrLn $ map (buildRow) part2
 
 
 
